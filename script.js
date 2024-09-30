560668bcf7a3a35cea8622931c99920388f8dfed
Rohan <rathodrohan721@gmail.com>
2024-09-30 10:41:55 +0530

    test

---
+++ script.js
@@ -0,0 +1,12 @@
+const text1= document.getElementById("t1")
+const text2= document.getElementById("t2")
+const text3= document.getElementById("t3")
+const btn= document.getElementById("btn1")
+
+text1.textContent
+btn.addEventListener("click",()=>{
+    alert(`
+        Name : ${text1.value}
+        Email : ${text2.value}
+        Query : ${text3.value}`)
+})
