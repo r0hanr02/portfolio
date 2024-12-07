0e8097b6736c248fb15ca482372fa6497ac6a61b
Rohan <rathodrohan721@gmail.com>
2024-12-07 17:04:11 +0530

    portFolio with Tailwind

--- script.js
+++ script.js
@@ -9,45 +9,6 @@
 //     }
 // });
 
-// let darkbtn= document.querySelector(".dark-mode")
-// let image = document.querySelector("#dark-image")
-// let check = 0
-// darkbtn.addEventListener("click",()=>{
-//     if (check == 0) {
-//         console.log("dark mode on");
-//         document.body.style.backgroundImage = "url('/icons/12 (4).webp')";
-//         image.src = "/icons/dark_mode_512dp.webp";
-//         darkbtn.style.background="white"
-//         image.style.filter="none"
-//         check = 1;
-//     } else {
-//         console.log("dark mode off");
-//         document.body.style.backgroundImage =  "url('/twoimage.webp')"; 
-//         image.src = "/icons/light_mode_512.webp";
-//         darkbtn.style.background="#212121"
-//         image.style.filter="none"
-//         check = 0;
-//     }}
-// )
-
-// let skills = document.querySelector("#skills")
-// let content = document.querySelector("#content")
-
-// skills.addEventListener("onclick",()=>{
-//     console.log("hello");
-//     content.innerHTML +=`
-//     <li>
-//         <span>Web Developer
-//         </span>
-//         <br>"building Website"
-//     </li>
-//     <li>
-//         <span>UI/UX</span>
-//         <br>
-//         "Design Web User-Friendly Interface"
-//     </li>`
-// })
-// Function to handle tab switching
 function opentab(tabname, event) {
     // Get all elements with class "tab-links" and "tab-contents"
     var tablinks = document.getElementsByClassName("tab-links");
@@ -55,12 +16,12 @@ function opentab(tabname, event) {
 
     // Remove "active-link" class from all tab links
     for (var i = 0; i < tablinks.length; i++) {
-        tablinks[i].classList.remove("active-link");
+      tablinks[i].classList.remove("active-link");
     }
 
     // Remove "active-tab" class from all tab contents
     for (var i = 0; i < tabcontents.length; i++) {
-        tabcontents[i].classList.remove("active-tab");
+      tabcontents[i].classList.remove("active-tab");
     }
 
     // Add "active-link" class to the clicked tab link
@@ -68,4 +29,33 @@ function opentab(tabname, event) {
 
     // Add "active-tab" class to the corresponding tab content
     document.getElementById(tabname).classList.add("active-tab");
-}
+  }
+
+const sr = ScrollReveal({
+  distance: '50px',
+  duration: 500,
+  easing: 'ease-in-out',
+  reset: true,
+});
+
+sr.reveal('.reveal', {
+  origin: 'bottom',
+  interval: 200,
+});
+
+const menuIcon = document.getElementById("menu-icon");
+const navbarLinks = document.getElementById("navbar-links");
+const menuItems = document.querySelectorAll(".menu-item");
+
+// Toggle the menu visibility on menu icon click
+menuIcon.addEventListener("click", () => {
+  const isMenuOpen = navbarLinks.style.right === "0px";
+  navbarLinks.style.right = isMenuOpen ? "-300px" : "0px";
+});
+
+// Close the menu when a menu item is clicked
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    navbarLinks.style.right = "-300px";
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'text-lg': '2px 2px 6px rgba(0, 0, 0, 0.5)', // Custom text shadow
+        'text-md': '1px 1px 4px rgba(0, 0, 0, 0.4)',
+        'text-sm': '1px 1px 2px rgba(0, 0, 0, 0.2)',
+      },
+      colors: {
+        custom: {
+          light: '#F0D2AD',
+          dark: '#3B2318',
+          accent: '#DCA87B',
+          secondaryDark: '#4A2718',
+          secondaryAccent: '#78462B',
+        },
+      },
+      backgroundImage: {
+        'gradient-to-b': 'linear-gradient(to bottom, var(--tw-gradient-stops))',
+        'gradient-to-r': 'linear-gradient(to right, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
+
+
+
+
